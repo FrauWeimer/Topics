b8e0d0b105663dd52239f2726e87517ae7eefd4b
Margarita Weimer <106189823+FrauWeimer@users.noreply.github.com>
2022-11-06 21:03:13 +0100

    Add files via upload

---
+++ def.py
@@ -0,0 +1,12 @@
+def multiply(a, b):
+    print("a = ", a)
+    print("b = ", b)
+    result = a*b
+    return result
+
+c = multiply(3, 5)
+print(c)
+c = multiply(7, 5)
+print(c)
+c = multiply(3, 55)
+print(c)
